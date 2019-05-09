e5cd89eb99f0ade67ffb6c31122609cce135ba9e
chingaloEric <echingalo@gmail.com>
2019-05-09 12:09:23 +0300

    added the reactive forms

--- src/app/components/log-in/log-in.component.ts
+++ src/app/components/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-
+  user: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.user = new FormGroup({
+      name: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+
+  onSubmit(user: any) {
+    console.log('welcome', user.value.name);
   }
 
 }
