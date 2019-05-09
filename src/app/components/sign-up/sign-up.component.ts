5eea88244d992933b21f085b0d03d30780e19b0c
chingaloEric <echingalo@gmail.com>
2019-05-09 13:26:05 +0300

    added the validation build up

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
+  user: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.user = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      phone: new FormControl(''),
+      password: new FormGroup({
+        pass1: new FormControl(''),
+        pass2: new FormControl('')
+      })
+    })
   }
 
 }
