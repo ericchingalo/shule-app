4cec8bd88e958bd416074b26799dd48fece9fe63
chingaloEric <echingalo@gmail.com>
2019-05-20 17:28:02 +0300

    fixed the app layout

--- src/app/pages/schools/schools.routing.module.ts
+++ src/app/pages/schools/schools.routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SchoolsByCategoryComponent } from './schools-by-category/schools-by-category.component';
 import { SchoolsByNameComponent } from './schools-by-name/schools-by-name.component';
+import { SchoolsComponent } from './schools/schools.component';
 const routes: Routes = [
+    {path: 'schools', component: SchoolsComponent},
     {path: 'schools/category', component: SchoolsByCategoryComponent},
     {path: 'schools/name', component: SchoolsByNameComponent}
 ];
